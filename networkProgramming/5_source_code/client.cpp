a32c362111bca16b63a174a49645abe466ca19e2
sency90 <sency90@gmail.com>
2015-10-29 11:05:05 +0900

    update

---
+++ networkProgramming/5_source_code/client.cpp
@@ -0,0 +1,11 @@
+# include "header.h"
+//pthread를 위한 헤더를 추가하시오
+void randomMessage (int id, int connectFD){
+    //writevn, readvn으로 두 랜덤 넘버를 전송하고 그 합을 받으시오.
+}
+
+int main(){
+    //순차적으로 ID를 생성하면서 10개의 클라이언트 프로세스를 fork하시오.
+    //자식 프로세스일 경우 서버와 접속하여 통신을 10번 반복한 후 접속을 끊으시오
+    //부모 프로세스일 경우 10개의 자식 프로세스가 끝나기를 기다리시오
+}"
---
+++ networkProgramming/5_source_code/server.cpp
@@ -0,0 +1,10 @@
+# include "header.h"
+//pthread를 위한 헤더를 추가하시오
+void* workerThreadFunction(void *arguments){
+    //readvn과 writevn을 이용해 클라이언트가 보낸 랜덤 넘버를 더해 되돌려보낸다
+    //클라이언트 ID를 포함해 받은 메시지와 두 랜덤 넘버의 합을 출력하시오
+}
+int main() {
+    //클라이언트가 접속하면 쓰레드를 새로 생성하여
+    //위 workerThreadFunction를 실행하도록 하시오
+}"
