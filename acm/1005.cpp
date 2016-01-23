195dcf4ab8acc2bd258cb5a6ea6827abcbe10f68
sency90 <sency90@gmail.com>
2016-01-23 23:56:19 +0900

    update acm

--- acm/1005.cpp
+++ acm/1005.cpp
@@ -5,57 +5,47 @@
 #include <limits.h>
 using namespace std;
 
-int t, n, k;
-int i, j;
 int w;
-int cost[1001];
-bool isDept[1001];
-int costFromCurToW[1001];
-queue<int> q[100001];
-
-void clear( queue<int> &q )
-{
-    queue<int> empty;
-    swap( q, empty );
-}
+int cost[1002];
+int costFromCurToW[1002];
+queue<int> q[100002];
 
 int calc( int x ) {
     int y = 0;
     int tempCost, tempRet;
     bool everEntered = false;
-
+    
     //base case
     if( x == w ) {
         //clear(q[x]);
         return cost[w];
     }
-
+    
     //recursive part
     while(!q[x].empty()) {
         everEntered = true;
-
+        
         y = q[x].front(); q[x].pop();
-
+        
         if(costFromCurToW[y] < 0) {
             tempRet = calc(y);
         }
         else {
             tempRet = costFromCurToW[y];
         }
-
+        
         if(tempRet < 0) {
             tempCost = -1;
         }
         else {
             tempCost = cost[x] + tempRet;
         }
-
+        
         if(costFromCurToW[x] < tempCost) {
             costFromCurToW[x] = tempCost;
         }
     }
-
-    //clear(q[x]);
+    
     if(everEntered) {
         return costFromCurToW[x];
     } else {
@@ -65,32 +55,39 @@ int calc( int x ) {
 
 int main() {
     int x, y;
+    int t, n, k;
+    bool isStartNode[1002];
+    
     scanf("%d", &t);
-    for(j=0; j<t; j++) {
-        memset(cost, 0, sizeof(cost));
-        memset(isDept, true, sizeof(isDept));
-        memset(costFromCurToW, -1, sizeof(costFromCurToW));
+    for(int j=0; j<t; j++) {
         scanf("%d", &n);
         scanf("%d", &k);
-
-        for(i=1; i<n+1; i++) {
+        memset(cost, 0, (n+2)*sizeof(int));
+        memset(isStartNode, true, (n+2)*sizeof(bool));
+        memset(costFromCurToW, -1, (n+2)*sizeof(int));
+        
+        for(int i=1; i<n+1; i++) {
             scanf("%d", &cost[i]);
         }
-        for(i=1; i<k+1; i++) {
+        for(int i=1; i<k+1; i++) {
             scanf("%d %d", &x, &y);
-            isDept[y] = false;
+            isStartNode[y] = false;
             q[x].push(y);
         }
-
+        
         scanf("%d", &w);
         costFromCurToW[w] = cost[w];
-
-        for(i=1; i<n+1; i++) {
-            if(isDept[i]) {
+        
+        for(int i=1; i<n+1; i++) {
+            if(isStartNode[i]) {
                 q[0].push(i);
             }
         }
         printf("%d\n", calc(0));
+        for(int ii=0; ii<k+1; ii++) {
+            queue<int> empty;
+            swap( q[ii], empty );
+        }
     }
     return 0;
 }
