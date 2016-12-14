ab7754171e82f128991e639e43292e9bde515b51
Lian Yang <sency90@gmail.com>
2016-12-14 21:10:08 +0900

    update

---
+++ xcode_ws/swift_test/swift_test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift_test
+//
+//  Created by Lian Yang on 11/11/2016.
+//  Copyright © 2016 Lian Yang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
