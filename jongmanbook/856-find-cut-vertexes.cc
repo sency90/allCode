39b3bbedb5cf6f17a9fbfcc01a0f305c587155af
Lian Yang <sency90@gmail.com>
2017-06-27 01:26:55 +0900

    plzrunoj update

--- jongmanbook/856-find-cut-vertexes.cc
+++ jongmanbook/856-find-cut-vertexes.cc
@@ -3,30 +3,29 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 vector<vector<int> > v;
 vector<bool> isCutVertex;
 vector<int> discovered;
 int counter=0;
 int dfs2(int x) {
-    discovered[x] = ++counter;
-    int children=0, mn=discovered[x];
-    for(int i=0; i<v[x].size(); i++) {
-        int y = v[x][i];
-        if(discovered[y]) {
-            mn = min(mn, discovered[y]);
-            continue;
+    int children=0, mn=discovered[x]=++counter;
+    for(int y: v[x]) {
+        if(discovered[y]) mn = min(mn, discovered[y]);
+        else {
+            children++;
+            int subtree = dfs2(y);
+            if(discovered[x]!=1 && subtree >= discovered[x]) isCutVertex[x] = true;
+            mn = min(mn, subtree);
         }
-        children++;
-        int subtree = dfs2(y);
-        if(discovered[x]!=1 && subtree >= discovered[x]) isCutVertex[x] = true;
-        mn = min(mn, subtree);
     }
-    if(discovered[x] == 1) { //if x is the root
-        isCutVertex[x] = (children >= 2);
-        return discovered[x];
-    } else return mn;
+    if(discovered[x]==1 && children>=2) { //if x is the root
+        isCutVertex[x] = true;
+    }
+    return mn;
 }
+void e(int x, int y) { v[x].push_back(y); v[y].push_back(x); }
 int main() {
     int n=7;
     v.resize(n, vector<int>());
@@ -34,6 +33,11 @@ int main() {
     isCutVertex.resize(n, false);
 
     //undirected graph
+    /*
+       e(0,1);
+       e(1,2);
+       e(2,0);
+       */
     v[0].push_back(1); v[1].push_back(0);
     v[0].push_back(4); v[4].push_back(0);
     v[0].push_back(5); v[5].push_back(0);
--- jongmanbook/856.cc
+++ jongmanbook/856.cc
@@ -4,25 +4,24 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-const int inf = 0x3f3f3f3f;
 vector<vector<int> > v;
 vector<int> discov,cv;
 int cnt=0;
 int dfs(int x) {
-    discov[x]=++cnt;
-    int ret = inf;
+    int ret=discov[x]=++cnt, tEdge=0;
     for(int y: v[x]) {
         if(discov[y]) ret = min(ret,discov[y]);
         else {
+            tEdge++;
             int subtree=dfs(y);
             //>= 이 부등호가 정말 중요하다. =빼먹으면 안됨. (bridge와 cut-vertex의 차이)
             if(discov[x]!=1 && subtree >= discov[x]) cv.push_back(x);
             ret = min(ret,subtree);
         }
     }
-    if(discov[x]==1) if(v[x].size()>=2) cv.push_back(x);
-
-    return min(ret,discov[x]);
+    //v[x].size()로 tEdge를 대체할 수 없음. treeEdge의 개수는 v[x].size()가 아니기 때문.
+    if(discov[x]==1 && tEdge>=2) cv.push_back(x);
+    return ret;
 }
 void e(int x, int y) {
     v[x].push_back(y); v[y].push_back(x);
@@ -33,17 +32,30 @@ int main() {
     discov.resize(n, 0);
 
     //undirected graph
+    /*
+       e(0,1);
+       e(1,2);
+       e(2,0);
+       */
     e(0,1);
+    e(0,4);
+    e(0,5);
+    e(0,6);
+    e(5,3);
+    e(5,6);
+    e(6,3);
+    e(4,2);
     e(1,2);
+    e(2,0);
+
 
     //check cut vertex
     dfs(0);
 
-    puts("Cut Vertexes:");
+    printf("Cut Vertexes: ");
     for(int p: cv) {
-        printf("%d\n", p);
-    }
+        printf("%d ", p);
+    } puts("");
 
     return 0;
 }
-
