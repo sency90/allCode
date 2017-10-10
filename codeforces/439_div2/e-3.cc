07a0e18acb786b7f4a5075834f88d55a1f67391d
Lian Yang <sency90@gmail.com>
2017-10-11 01:37:30 +0900

    quadtree update

--- codeforces/439_div2/e-3.cc
+++ codeforces/439_div2/e-3.cc
@@ -17,7 +17,6 @@ map<piipii,int> mp;
 int n,m,q;
 int sx=1,sy=1,ex,ey,sz=1;
 vector<ll> lazy,tree;
-
 struct ano{
     operator ull(){
         ull x=0,y=0,c=getchar();
@@ -26,20 +25,14 @@ struct ano{
         return y?-x:x;
     }
 }buf;
-
-
 inline void pushdown(int r, int sx, int sy, int ex, int ey) {
     //if(lazy[r]) {
     if(!(sx==ex && sy==ey)) {
-        int a,b,c,d;
-
-        if(r==0) a=1,b=2,c=3,d=4;
-        else if(r&1) a=r4+1,b=r4+2,c=r4+5,d=r4+6;
-        else a=r4-1,b=r4,c=r4+3,d=r4+4;
-        lazy[a] += lazy[r];
-        lazy[b] += lazy[r];
-        lazy[c] += lazy[r];
-        lazy[d] += lazy[r];
+        int t=r4;
+        lazy[t+1] += lazy[r];
+        lazy[t+2] += lazy[r];
+        lazy[t+3] += lazy[r];
+        lazy[t+4] += lazy[r];
     } else tree[r] += lazy[r]; //단말노드에만 tree[r]값 갱신해준다.
     //tree[r]+=lazy[r]*(ex-sx+1)*(ey-sy+1); //근데 단말 노드가 아니면 이 정보가 필요가 없음.
     lazy[r]=0;
@@ -54,15 +47,10 @@ void update(int r, int sx, int sy, int ex, int ey, int qsx, int qsy, int qex, in
         //return tree[r];
     } else {
         int mx2=(sx+ex)>>1, my2=(sy+ey)>>1, t=r4;
-        int a,b,c,d;
-        if(r==0) a=1,b=2,c=3,d=4;
-        else if(r&1) a=r4+1,b=r4+2,c=r4+5,d=r4+6;
-        else a=r4-1,b=r4,c=r4+3,d=r4+4;
-
-        update(a, sx,sy, mx2,my2, qsx,qsy,qex,qey,by);
-        update(b, sx,my2+1, mx2,ey, qsx,qsy,qex,qey,by);
-        update(c, mx2+1,sy, ex,my2, qsx,qsy,qex,qey,by);
-        update(d, mx2+1,my2+1, ex,ey, qsx,qsy,qex,qey,by);
+        update(t+1, sx,sy, mx2,my2, qsx,qsy,qex,qey,by);
+        update(t+2, sx,my2+1, mx2,ey, qsx,qsy,qex,qey,by);
+        update(t+3, mx2+1,sy, ex,my2, qsx,qsy,qex,qey,by);
+        update(t+4, mx2+1,my2+1, ex,ey, qsx,qsy,qex,qey,by);
     }
 }
 ll query(int r, int sx, int sy, int ex, int ey, int qx, int qy) {
@@ -70,21 +58,17 @@ ll query(int r, int sx, int sy, int ex, int ey, int qx, int qy) {
     if(ex<qx || qx<sx || ey<qy || qy<sy) return 0; //tree[r];
     else if(sx==ex && sy==ey) return tree[r];
     else {
-        int mx2=(sx+ex)>>1, my2=(sy+ey)>>1;
-        int a,b,c,d;
-        if(r==0) a=1,b=2,c=3,d=4;
-        else if(r&1) a=r4+1,b=r4+2,c=r4+5,d=r4+6;
-        else a=r4-1,b=r4,c=r4+3,d=r4+4;
-
-        ll u=query(a,sx,sy,mx2,my2,qx,qy);
-        ll v=query(b,sx,my2+1,mx2,ey,qx,qy);
-        ll w=query(c,mx2+1,sy,ex,my2,qx,qy);
-        ll z=query(d,mx2+1,my2+1,ex,ey,qx,qy);
-        return u+v+w+z;
+        int mx2=(sx+ex)>>1, my2=(sy+ey)>>1, t=r4;
+        ll a=query(t+1,sx,sy,mx2,my2,qx,qy);
+        ll b=query(t+2,sx,my2+1,mx2,ey,qx,qy);
+        ll c=query(t+3,mx2+1,sy,ex,my2,qx,qy);
+        ll d=query(t+4,mx2+1,my2+1,ex,ey,qx,qy);
+        return a+b+c+d;
     }
 }
 int main() {
     n=buf;m=buf;q=buf;
+    //scanf("%d%d%d",&n,&m,&q);
     int i;
     sx=sy=ex=ey=sz=i=1;
     for(;i<max(n,m);i<<=1) sz+=i*i;
@@ -95,6 +79,7 @@ int main() {
     tree.resize(sz); lazy.resize(sz);
     for(int i=0; i<q; i++) {
         int t=buf,x1=buf,y1=buf,x2=buf,y2=buf;
+        //scanf("%d%d%d%d%d",&t,&x1,&y1,&x2,&y2);
         if((x1==x2 && y1>y2) || x1>x2) swap(x1,x2),swap(y1,y2);
         if(t==1) {
             int val = (rand()<<15)|rand();
