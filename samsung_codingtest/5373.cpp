0b3bd3920cfe329bb78ac154b6658385dcc65c8e
sency90 <sency90@gmail.com>
2025-06-03 23:28:45 +0900

    solved boj 15686, 5373

--- samsung_codingtest/5373.cpp
+++ samsung_codingtest/5373.cpp
@@ -43,30 +43,105 @@ struct Side{
 			}
 		}
 	}
+
+	void print() const {
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				printf("%c", arr[i][j]);
+			}
+			puts("");
+		}
+	}
 };
 
-void rotate_clockwise(Side **p) {
-	p[0]->rotate_right();
-	Side tmp = *p[0];
-	for(int i=1; i<4; i++) {
-		for(int j=0; j<3; j++) {
-			p[i]->arr[2][j] = p[i+1]->arr[2][j];
+int chk[6][4][9] = {
+	{
+		{3,2,1,0,0,0,0,0,0}, //B
+		{3,2,1,0,0,0,0,0,0}, //R
+		{3,2,1,0,0,0,0,0,0}, //F
+		{3,2,1,0,0,0,0,0,0}  //L
+	}, //U
+	{
+		{0,0,0,0,0,0,1,2,3}, //F
+		{0,0,0,0,0,0,1,2,3}, //R
+		{0,0,0,0,0,0,1,2,3}, //B
+		{0,0,0,0,0,0,1,2,3}  //L
+	}, //D
+	{
+		{0,0,0,0,0,0,1,2,3}, //U
+		{1,0,0,2,0,0,3,0,0}, //R
+		{3,2,1,0,0,0,0,0,0}, //D
+		{0,0,3,0,0,2,0,0,1}  //L
+	}, //F
+	{
+		{3,2,1,0,0,0,0,0,0}, //U
+		{1,0,0,2,0,0,3,0,0}, //L
+		{0,0,0,0,0,0,1,2,3}, //D
+		{0,0,3,0,0,2,0,0,1}  //R
+	}, //B
+	{
+		{1,0,0,2,0,0,3,0,0}, //U
+		{1,0,0,2,0,0,3,0,0}, //F
+		{1,0,0,2,0,0,3,0,0}, //D
+		{0,0,3,0,0,2,0,0,1}  //B
+	}, //L
+	{
+		{0,0,3,0,0,2,0,0,1}, //U
+		{1,0,0,2,0,0,3,0,0}, //B
+		{0,0,3,0,0,2,0,0,1}, //D
+		{0,0,3,0,0,2,0,0,1}  //F
+	} //R
+};
+
+int tmp[4][4];
+void rotate_clockwise(Side **p, int side_no) {
+	p[4]->rotate_right();
+	for(int z=0; z<4; z++) {
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				int c = chk[side_no][z][i*3+j];
+				if(c) {
+					tmp[z][c] = p[z]->arr[i][j];
+				}
+			}
 		}
 	}
-	for(int j=0; j<3; j++) {
-		p[4]->arr[2][j] = tmp.arr[2][j];
+
+	for(int z=0; z<4; z++) {
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				int c = chk[side_no][z][i*3+j];
+				if(c) {
+					p[z]->arr[i][j] = tmp[(z-1+4)%4][c];
+				}
+			}
+		}
 	}
 }
-void rotate_counterclockwise(Side **p) {
-	p[0]->rotate_left();
-	Side tmp = *p[4];
-	for(int i=4; i>=1; i--) {
-		for(int j=0; j<3; j++) {
-			p[i]->arr[2][j] = p[i-1]->arr[2][j];
+void rotate_counterclockwise(Side **p, int side_no) {
+	p[4]->rotate_left();
+	for(int z=0; z<4; z++) {
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				int c = chk[side_no][z][i*3+j];
+				if(c) {
+					//printf("[z:%d](%d,%d) ", z,i,j);
+					tmp[z][c] = p[z]->arr[i][j];
+				}
+			}
 		}
+		//puts("");
 	}
-	for(int j=0; j<3; j++) {
-		p[0]->arr[2][j] = tmp.arr[2][j];
+
+	for(int z=0; z<4; z++) {
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				int c = chk[side_no][z][i*3+j];
+				if(c) {
+					p[z]->arr[i][j] = tmp[(z+1)%4][c];
+				}
+			}
+		}
 	}
 }
 Side cube[6];
@@ -86,12 +161,12 @@ constexpr int R = 5;
 
 char dir_names[] = {'U', 'D', 'F', 'B', 'L', 'R'};
 int side_order[6][5] = {
-	{ U,B,R,F,L },
-	{ D,F,R,B,L },
-	{ F,U,R,D,L },
-	{ B,D,R,U,L },
-	{ L,U,F,D,B },
-	{ R,U,B,D,F }
+	{ B,R,F,L,U },
+	{ F,R,B,L,D },
+	{ U,R,D,L,F },
+	{ U,L,D,R,B },
+	{ U,F,D,B,L },
+	{ U,B,D,F,R }
 };
 
 char order[256];
@@ -108,7 +183,7 @@ char cmd[3];
 int main() {
 	int tc; scanf("%d", &tc);
 	for(int i=0; i<6; i++) {
-		order[dir_names[i]] = i;
+		order[(int)dir_names[i]] = i;
 	}
 
 	Side *p[5];
@@ -120,16 +195,10 @@ int main() {
 			for(int j=0; j<5; j++) {
 				p[j] = &cube[side_order[order[cmd[0]]][j]];
 			}
-			if(cmd[1] == '+') rotate_clockwise(p);
-			else rotate_counterclockwise(p);
+			if(cmd[1] == '+') rotate_clockwise(p, order[cmd[0]]);
+			else rotate_counterclockwise(p, order[cmd[0]]);
 		}
-
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
-				printf("%c", p[U]->arr[i][j]);
-			}
-			puts("");
-		}
+		cube[U].print();
 	}
 	return 0;
 }
